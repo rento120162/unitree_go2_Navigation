50caad0c982929ee4d0a938bcb29dab2aa661283
rento120162 <r120162@g.kobe-kosen.ac.jp>
2026-04-24 11:54:39 +0000

    third

--- FAST_LIO_ROS2/src/frame_changer.cpp
+++ FAST_LIO_ROS2/src/frame_changer.cpp
@@ -1,7 +1,8 @@
 #include <memory>
 
-#include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/odometry.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 
 class OdomFrameFixer : public rclcpp::Node
 {
@@ -9,33 +10,39 @@ public:
     OdomFrameFixer()
     : Node("odom_frame_fixer")
     {
-        sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-            "/Odometry",
-            10,
-            std::bind(&OdomFrameFixer::callback, this, std::placeholders::_1)
-        );
+        odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("/Odometry",10,std::bind(&OdomFrameFixer::odomcallback, this, std::placeholders::_1));
+        pc2_sub_ = this->create_subscription<sensor_msgs::msg::PointCloud2>("/cloud_registed",10,std::bind(&OdomFrameFixer::pc2callback, this, std::placeholders::_1));
 
-        pub_ = this->create_publisher<nav_msgs::msg::Odometry>(
-            "/lio_odom_fixed",
-            10
-        );
+        odom_pub_ = this->create_publisher<nav_msgs::msg::Odometry>("/lio_odom_fixed",10);
+        pc2_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("/cloud_registed_fixed",10);
 
         RCLCPP_INFO(this->get_logger(), "odom_frame_fixer started");
     }
 
 private:
-    void callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+    void odomcallback(const nav_msgs::msg::Odometry::SharedPtr msg)
     {
         auto odom = *msg;
 
         odom.header.frame_id = "odom";
         odom.child_frame_id = "base_footprint";
 
-        pub_->publish(odom);
+        odom_pub_->publish(odom);
     }
 
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr sub_;
-    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr pub_;
+    void pc2callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+    {
+        auto pc2 = *msg;
+
+        pc2.header.frame_id = "odom";
+
+        pc2_pub_->publish(pc2);
+    }
+
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr pc2_sub_;
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pc2_pub_;
 };
 
 int main(int argc, char ** argv)
